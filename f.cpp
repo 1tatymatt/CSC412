3cce5bd74fcdd288eed196dad3e4e9b3408f139f
Taty Matt <TatyRosie@Tatys-MacBook-Pro.local>
2016-09-02 13:37:21 -0400

    Added changes

--- f.cpp
+++ f.cpp
@@ -1,2 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+
 int x = 1, y = 2;
 int z;
+z = x + y;
+cout<< "The total is " << z << endl;
+
+return 0;
+
+}
---
+++ f_2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+cout<< "Hello World, Taty is here!" <<endl;
+
+return 0;
+
+}
